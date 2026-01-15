334d589870e1cc6e0631fd457990941f0a0c5401
D327x <dens241235@gmail.com>
2026-01-15 10:47:22 +0100

    Implementation BRAM user (non testé)

--- MLP/MLP.runs/synth_1/rundef.js
+++ MLP/MLP.runs/synth_1/rundef.js
@@ -2,16 +2,20 @@
 // Vivado(TM)
 // rundef.js: a Vivado-generated Runs Script for WSH 5.1/5.6
 // Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
-// Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+// Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 //
+
+echo "This script was generated under a different operating system."
+echo "Please update the PATH variable below, before executing this script"
+exit
 
 var WshShell = new ActiveXObject( "WScript.Shell" );
 var ProcEnv = WshShell.Environment( "Process" );
 var PathVal = ProcEnv("PATH");
 if ( PathVal.length == 0 ) {
-  PathVal = "C:/Program/2025.2/Vitis/bin;C:/Program/2025.2/Vivado/ids_lite/ISE/bin/nt64;C:/Program/2025.2/Vivado/ids_lite/ISE/lib/nt64;C:/Program/2025.2/Vivado/bin;";
+  PathVal = "/usr/local/Xilinx/Vitis/2024.2/bin:/usr/local/Xilinx/Vivado/2024.2/ids_lite/ISE/bin/lin64;/usr/local/Xilinx/Vivado/2024.2/bin;";
 } else {
-  PathVal = "C:/Program/2025.2/Vitis/bin;C:/Program/2025.2/Vivado/ids_lite/ISE/bin/nt64;C:/Program/2025.2/Vivado/ids_lite/ISE/lib/nt64;C:/Program/2025.2/Vivado/bin;" + PathVal;
+  PathVal = "/usr/local/Xilinx/Vitis/2024.2/bin:/usr/local/Xilinx/Vivado/2024.2/ids_lite/ISE/bin/lin64;/usr/local/Xilinx/Vivado/2024.2/bin;" + PathVal;
 }
 
 ProcEnv("PATH") = PathVal;
---
+++ MLP/MLP.srcs/sources_1/new/BRAM_user.vhd
@@ -0,0 +1,59 @@
+----------------------------------------------------------------------------------
+-- Company: INSA
+-- Engineer: CARVALHO - LESPIAUCQ
+-- 
+-- Create Date: 10/16/2024 05:53:22 PM
+-- Design Name: 
+-- Module Name: BancMemoire - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+--use IEEE.std_logic_arith.ALL;
+use IEEE.std_logic_unsigned.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity BRAM_user is
+    Port ( addr : in STD_LOGIC_VECTOR (7 downto 0);
+           CLK : in STD_LOGIC;
+           data_out : out STD_LOGIC_VECTOR (31 downto 0));
+end BRAM_user;
+
+architecture Behavioral of BRAM_user is
+    type mem_type is array(0 to 255) of STD_LOGIC_VECTOR(31 downto 0);
+    signal data_mem : mem_type := (
+    0 => X"30000000",-- = 1.5     
+    1 => X"10000000",-- = 0.5     
+    2 => X"A0000000",-- = -1.0    
+    3 => X"40000000",-- =  2.0    
+    4 => X"BE000000",-- =  -1.9375
+    5 => X"D1EB851E",-- =  -2.56  
+    others => X"00"
+    );
+begin
+    process(CLK)
+    begin
+        if rising_edge(CLK) then
+            data_out <= data_mem(TO_INTEGER (unsigned (addr)));
+        end if;
+    end process;
+end Behavioral;
